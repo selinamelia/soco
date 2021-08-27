ecceb955b6224830622a19b119ca4825506ac669
selinamelia <sellinaamelia@gmail.com>
2021-08-27 23:46:43 +0700

    add seeder and config file

---
+++ seeder/seeder.rb
@@ -0,0 +1,9 @@
+pages << {
+    url: 'https://www.sociolla.com/2277-face-wash',
+    page_type: "listings",
+    fetch_type: 'browser',
+    force_fetch: true,
+    method: "GET",
+    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    
+}
